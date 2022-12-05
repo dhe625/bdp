155e86477a675f0f6871e9de8ece54d25104a7be
Woo SangDeok <woosd97@naver.com>
2022-12-06 00:11:34 +0900

    add API python code by deok

---
+++ Jeju_Parking_lot_API.py
@@ -0,0 +1,16 @@
+#모듈을 추가한다.
+import requests
+import pandas as pd
+#제주도내 주차장 기본정보 API를 받아온다.
+url = "http://api.jejuits.go.kr/api/infoParkingInfoList?code=860538"
+json_obj = requests.get(url).json()
+json_obj
+#list를 만들어 데이터프레임화 하고 head를 찍어보았다.
+list = []
+for infos in json_obj['info']:
+    if infos.get('id'):
+        list.append([infos['name'],infos['addr'],infos['x_crdn'],infos['y_crdn']])
+df = pd.DataFrame(list, columns = ['name', 'addr', 'x_crdn', 'y_crdn'])
+df.head()
+#CSV 파일로 저장한다.
+df.to_csv('C:/Deok/Jeju_parkinglot.csv', header=True, index=False)
