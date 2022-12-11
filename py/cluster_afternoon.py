96eb1d032febac8aca8855dfed553412b75e6da0
= <dhe625@mju.ac.kr>
2022-12-11 20:51:34 +0900

    Update py, csv

--- py/cluster_afternoon.py
+++ py/cluster_afternoon.py
@@ -10,23 +10,33 @@ if __name__ == "__main__":
 	vecAssembler = VectorAssembler(inputCols=["latitude", "longitude"], outputCol="features")
 	new_df = vecAssembler.transform(df)
 	
-	kmeans = KMeans(k=10, seed=1)
-	model = kmeans.fit(new_df.select('features'))
-
-	transformed = model.transform(new_df)
+	silhouette_scores=[]
+	evaluator = ClusteringEvaluator(featuresCol='features', metricName='silhouette')
+   	
+	for K in range(2,15):
+		KMeans_=KMeans(featuresCol='features', k=K)
+		KMeans_fit=KMeans_.fit(new_df)
+		KMeans_transform=KMeans_fit.transform(new_df)
+		evaluation_score=evaluator.evaluate(KMeans_transform)
+		silhouette_scores.append(evaluation_score)
 	
-	print("Clusters of afternoon")
-
-	evaluator = ClusteringEvaluator()                                                                            
-	silhouette = evaluator.evaluate(transformed)
-	print("Silhouette with squared euclidean distance = " + str(silhouette))
+	for i in range(len(silhouette_scores)):
+		print(i+2, "clusters:", silhouette_scores[i])
+	
+	kmeans = KMeans(k=13, seed=1)
+	model = kmeans.fit(new_df.select('features'))
 
 	centers = model.clusterCenters()
 	
 	sc = spark.sparkContext
 	centroids = sc.parallelize(centers).map(lambda x: [float(i) for i in x]).toDF(["latitude", "longitude"])
-	centroids.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clusterCentroids_afternoon")
+	centroids.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/centroids_afternoon")
+	
+	print("Clusters of afternoon")
+	
+	transformed = model.transform(new_df)
 
+	result0 = transformed.filter("prediction == 0").select('longitude','latitude')
 	result1 = transformed.filter("prediction == 1").select('longitude','latitude')
 	result2 = transformed.filter("prediction == 2").select('longitude','latitude')
 	result3 = transformed.filter("prediction == 3").select('longitude','latitude')
@@ -36,15 +46,21 @@ if __name__ == "__main__":
 	result7 = transformed.filter("prediction == 7").select('longitude','latitude')
 	result8 = transformed.filter("prediction == 8").select('longitude','latitude')
 	result9 = transformed.filter("prediction == 9").select('longitude','latitude')
-	result0 = transformed.filter("prediction == 0").select('longitude','latitude')
+	result10 = transformed.filter("prediction == 10").select('longitude','latitude')
+	result11 = transformed.filter("prediction == 11").select('longitude','latitude')
+	result12 = transformed.filter("prediction == 12").select('longitude','latitude')
 	
-	result1.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/1_afternoon")
-	result2.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/2_afternoon")
-	result3.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/3_afternoon")
-	result4.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/4_afternoon")
-	result5.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/5_afternoon")
-	result6.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/6_afternoon")
-	result7.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/7_afternoon")
-	result8.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/8_afternoon")
-	result9.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/9_afternoon")
-	result0.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/0_afternoon")
+	result0.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/0_afternoon")
+	result1.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/1_afternoon")
+	result2.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/2_afternoon")
+	result3.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/3_afternoon")
+	result4.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/4_afternoon")
+	result5.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/5_afternoon")
+	result6.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/6_afternoon")
+	result7.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/7_afternoon")
+	result8.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/8_afternoon")
+	result9.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/9_afternoon")
+	result10.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/10_afternoon")
+	result11.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/11_afternoon")
+	result12.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_afternoon/12_afternoon")
+
--- py/cluster_morning.py
+++ py/cluster_morning.py
@@ -10,23 +10,33 @@ if __name__ == "__main__":
 	vecAssembler = VectorAssembler(inputCols=["latitude", "longitude"], outputCol="features")
 	new_df = vecAssembler.transform(df)
 	
-	kmeans = KMeans(k=10, seed=1)
+	silhouette_scores=[]
+	evaluator = ClusteringEvaluator(featuresCol='features', metricName='silhouette')
+	for K in range(2,15):
+		KMeans_=KMeans(featuresCol='features', k=K)
+		KMeans_fit = KMeans_.fit(new_df)
+		KMeans_transform=KMeans_fit.transform(new_df)
+		evaluation_score=evaluator.evaluate(KMeans_transform)
+		silhouette_scores.append(evaluation_score)
+	
+	for i in range(len(silhouette_scores)):
+		print(i+2, "clusters:", silhouette_scores[i])
+	
+	kmeans = KMeans(k=12, seed=1)
 	model = kmeans.fit(new_df.select('features'))
-
-	transformed = model.transform(new_df)
-	
-	print("Clusters of morning")
-	
-	evaluator = ClusteringEvaluator()                                                                            
-	silhouette = evaluator.evaluate(transformed)
-	print("Silhouette with squared euclidean distance = " + str(silhouette))
 
 	centers = model.clusterCenters()
 	
 	sc = spark.sparkContext
 	centroids = sc.parallelize(centers).map(lambda x: [float(i) for i in x]).toDF(["latitude", "longitude"])
-	centroids.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clusterCentroids_morning")
 	
+	centroids.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/centroids_morning")
+	
+	print("Clusters of morning")
+	
+	transformed = model.transform(new_df)
+
+	result0 = transformed.filter("prediction == 0").select('longitude','latitude')
 	result1 = transformed.filter("prediction == 1").select('longitude','latitude')
 	result2 = transformed.filter("prediction == 2").select('longitude','latitude')
 	result3 = transformed.filter("prediction == 3").select('longitude','latitude')
@@ -36,15 +46,18 @@ if __name__ == "__main__":
 	result7 = transformed.filter("prediction == 7").select('longitude','latitude')
 	result8 = transformed.filter("prediction == 8").select('longitude','latitude')
 	result9 = transformed.filter("prediction == 9").select('longitude','latitude')
-	result0 = transformed.filter("prediction == 0").select('longitude','latitude')
-	
-	result1.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/1_morning")
-	result2.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/2_morning")
-	result3.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/3_morning")
-	result4.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/4_morning")
-	result5.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/5_morning")
-	result6.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/6_morning")
-	result7.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/7_morning")
-	result8.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/8_morning")
-	result9.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/9_morning")
-	result0.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/0_morning")
+	result10 = transformed.filter("prediction == 10").select('longitude','latitude')
+	result11 = transformed.filter("prediction == 11").select('longitude','latitude')
+
+	result0.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/0_morning")
+	result1.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/1_morning")
+	result2.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/2_morning")
+	result3.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/3_morning")
+	result4.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/4_morning")
+	result5.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/5_morning")
+	result6.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/6_morning")
+	result7.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/7_morning")
+	result8.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/8_morning")
+	result9.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/9_morning")
+	result10.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/10_morning")
+	result11.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("hdfs:///user/maria_dev/projectData/clustered_morning/11_morning")
