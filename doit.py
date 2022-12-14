d615c5755a8e8106e4b2851c591c5e75dc00cb62
dajinikim <109938354+dajinikim@users.noreply.github.com>
2022-12-14 13:11:52 +0900

    Add files via upload
    월별 데이터의 oid 중복 개수를 MapReduce를 이용하여 count하는 코드 생성

---
+++ doit.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+class IdCount(MRJob):
+	def steps(self):
+		return [
+				MRStep(mapper = self.map_id_count,
+					reducer = self.reduce_id_count)
+				]
+	def map_id_count(self,_,line):
+		id, collection_dt, longitude, latitude, time, diff = line.split(',')
+		id = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ',id)
+		if id != 'id':
+			yield(id,1)
+
+
+	def reduce_id_count(self,key,values):
+		yield (key, sum(values))
+
+if __name__ == '__main__':
+	IdCount.run()
